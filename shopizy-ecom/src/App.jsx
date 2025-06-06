db29b4593ef9a482ab96d4f90110fd9486b8f9d1
VAbrishamii <vahide.abri@yahoo.com>
2025-06-06 11:48:14 +0200

    Update App.jsx

--- shopizy-ecom/src/App.jsx
+++ shopizy-ecom/src/App.jsx
@@ -6,7 +6,13 @@ import ProductPage from "./pages/ProductPage";
 import CheckoutPage from "./pages/checkOutPage";
 import ContactPage from "./pages/ContactPage";
 import WishlistPage from "./pages/WishListPage";
-
+/**
+ * 
+ * @returns App component that defines the main routes of the application.
+ * It uses React Router to set up the routing structure, including a layout component that wraps around the main content.
+ * The routes include the home page, cart page, product details page, checkout page, contact page, and wishlist page.
+ * The Layout component is used to provide a consistent header and footer across all pages.
+ */
 function App() {
   return (
     <Routes>
